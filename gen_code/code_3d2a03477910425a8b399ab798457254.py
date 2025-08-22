377679e1edb6059420de10ed2b65e1421dbd513c
sdnt <117097790+sdntgnsh@users.noreply.github.com>
2025-08-23 02:00:55 +0530

    Auto-Push

---
+++ gen_code/code_3d2a03477910425a8b399ab798457254.py
@@ -0,0 +1,18 @@
+s = input()
+pattern = input()
+
+n, m = len(s), len(pattern)
+res = -1
+
+for i in range(n - m + 1):
+    diff = 0
+    for j in range(m):
+        if s[i + j] != pattern[j]:
+            diff += 1
+            if diff > 1:
+                break
+    if diff <= 1:
+        res = i
+        break
+
+print(res)
