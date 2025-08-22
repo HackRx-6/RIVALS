c1dcd5852a03ce848ceb19afbb378478983cc4f4
sdnt <117097790+sdntgnsh@users.noreply.github.com>
2025-08-23 02:36:57 +0530

    Auto-Push

---
+++ round_6/code_fa735128aff245b481465edcdb969a39.py
@@ -0,0 +1,17 @@
+def almost_equal_substring(s, pattern):
+    n = len(s)
+    m = len(pattern)
+    for i in range(n - m + 1):
+        diff = 0
+        for j in range(m):
+            if s[i + j] != pattern[j]:
+                diff += 1
+                if diff > 1:
+                    break
+        if diff <= 1:
+            return i
+    return -1
+
+s = input()
+pattern = input()
+print(almost_equal_substring(s, pattern))
