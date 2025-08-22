04397de0a61272a13fc12b75f4401b5e2540b67d
sdnt <117097790+sdntgnsh@users.noreply.github.com>
2025-08-23 02:49:35 +0530

    Auto-Push

--- round_6/code_6fde758d8ea244eb96d6f024cd186b8f.py
+++ round_6/code_6fde758d8ea244eb96d6f024cd186b8f.py
@@ -3,12 +3,8 @@ pattern = input()
 n, m = len(s), len(pattern)
 res = -1
 for i in range(n - m + 1):
-    diff = 0
-    for j in range(m):
-        if s[i + j] != pattern[j]:
-            diff += 1
-            if diff > 1:
-                break
+    sub = s[i:i+m]
+    diff = sum(1 for a, b in zip(sub, pattern) if a != b)
     if diff <= 1:
         res = i
         break
